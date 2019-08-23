054c63c01e6327c63c84aba05eb02c554b886e57
Tan Zhen Rong Eugene (IFAP IT DSA BIA) <ZhenRongEugene.Tan@infineon.com>
2019-08-23 15:57:55 +0800

    workshop04

--- workshop04/main.js
+++ workshop04/main.js
@@ -20,7 +20,8 @@ const express = require('express')
 const CitiesDB = require('./citiesdb');
 
 const serviceId = uuid().substring(0, 8);
-const serviceName = `zips-${serviceId}`
+//const serviceName = `cities-${serviceId}`
+const serviceName = 'cities'
 
 //Load application keys
 //Rename _keys.json file to keys.json
@@ -32,8 +33,8 @@ console.info(`Using ${keys.mongo}`);
 // if they are not the defaults below
 const db = CitiesDB({  
 	connectionUrl: keys.mongo, 
-	databaseName: 'zips', 
-	collectionName: 'city'
+	databaseName: 'cities', 
+	collectionName: 'cities'
 });
 
 const app = express();
@@ -47,12 +48,158 @@ app.use(express.urlencoded({ extended: true }));
 
 // Start of workshop
 
-// TODO 1/3 Load schemans
 
+// TODO 1/3 Load schemas
+// TODO 1/2 Load schemas
+const citySchema = require('./schema/city-schema.json')
+//const cityAPISpec = require('./schema/city-api.yaml')
+
+new OpenAPIValidator({
+	apiSpecPath: __dirname + '/schema/city-api.yaml'
+}).install(app)
 
 
 
 // TODO 2/3 Copy your routes from workshop03 here
+
+// Start of workshop
+// TODO 2/2 Copy your routes from workshop02 here
+
+// Mandatory workshop
+// TODO GET /api/states
+app.get(
+	'/api/states',
+	cacheControl({
+		maxAge: 10,
+		public: true
+	}),
+	(req, resp) => {
+		console.info('Processing /api/states', new Date());
+		db.findAllStates()
+			.then(result => {
+				resp.status(200);
+				resp.type('application/json');
+				resp.json(result.map(v => `/api/state/${v}`));
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error);
+			});
+		}
+)
+
+
+//TODO HEAD /api/state/:state
+app.head('api/state/:state', (req, resp) => {
+	//should check for state existence
+	resp.status(200)
+	resp.type('application/json')
+	resp.header('Accept-Ranges', 'cities')
+	resp.end();
+})
+
+
+// TODO GET /api/state/:state
+const etagFunction = {
+	stateAsync: (req) => {
+		const state = req.params.state.toLowerCase();
+		const p = new Promise(
+			(resolve, reject) => {
+				db.countCitiesInState(state)
+					.then(result => {
+						// etag signature -> ca123 or sg3 etc.
+						resolve(state + result);
+					})
+					.catch(error => {
+						reject(error);
+					})
+			}
+		);
+		return (p);
+	}
+}
+
+app.get(
+	'/api/state/:state', 
+	preconditions(etagFunction),
+	cacheControl({noCache: true}),
+	range({accept: 'cities', limit: 20}), //for pagination
+	(req, resp) => {
+	const state = req.params.state;
+	const first = req.range.first;
+	const last = req.range.last;
+	
+	Promise.all([
+		db.findCitiesByState(state, {limit: (last-first+1), offset: first}),
+		db.countCitiesInState(state)
+	]).then(results => {
+		//resp.status(200);
+		resp.status(206);
+		resp.type('application/json');
+		resp.range({
+			first: first,
+			last: last,
+			length: results[1]
+		});
+		resp.set('ETag', `"${state.toLowerCase()}${results[1]}"`)
+		resp.json(results[0].map(v => `/api/city/${v}`));
+	}).catch(error => {
+		resp.status(400);
+		resp.type('text/plain');
+		resp.send(error);
+	})
+})
+
+
+// TODO GET /api/city/:cityId
+app.get('/api/city/:cityId', (req, resp) => {
+	const cityId = req.params.cityId;
+
+	db.findCityById(cityId)
+		.then(result => {
+			if (result.length > 0) {
+				resp.status(200);
+				resp.type('application/json');
+				resp.json(result[0]);				
+			}
+			else {
+				resp.status(404);
+				resp.type('application/json');
+				resp.json({message: 'city not found'});				
+			}
+		})
+		.catch(error => {
+			resp.status(400);
+			resp.type('text/plain');
+			resp.send(error);
+		})
+})
+
+// TODO POST /api/city
+app.post(
+	'/api/city', 
+	schemaValidator.validate({body: citySchema}),
+	(req, resp) => {
+		const data = req.body
+		console.info('>> data:', data)
+
+		db.insertCity(data)
+			.then(result => {
+				resp.status(201)
+				resp.type('application/json')
+				resp.json({message: 'added'})
+			})
+			.catch(error => {
+				resp.status(400)
+				resp.type('text/plain')
+				resp.send(error)
+			})
+	}
+)
+
+
+
 
 
 
@@ -95,9 +242,37 @@ db.getDB()
 			console.info(`\tService id: ${serviceId}`);
 
 			// TODO 3/3 Add service registration here
-
-
-
+			console.info(`Registering Service: ${serviceName}: ${serviceId}`)
+			consul.agent.service.register({
+				id: serviceId,
+				name: serviceName,
+				port: PORT, 
+				check: {
+					interval: '10s', // listen from this service every 10 seconds
+					deregistercriticalserviceafter: '30s' // if not heard for 3 times, deregister (heartbeat)
+				}
+			})
+			.then(() => {
+				const intId = setInterval(
+					() => {
+						consul.agent.check.pass({
+							id: `${serviceName}: ${serviceId}`
+						})
+					},
+					10 * 1000 // 10 sec
+				)
+				process.on('SIGINT', 
+					() => {
+						console.info(`Deregistering Service: ${serviceName}: ${serviceId}`)
+						clearInterval(intId);
+						consul.agent.service.degregister({
+							id: `${serviceName}: ${serviceId}`							
+						}).finally(
+							() => process.exit()
+						)
+					}
+				)
+			})
 
 		});
 	})
